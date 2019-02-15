80afac5c6a3a4499fa90f7e0cee5c67adc883285
Deanosim <4834976+Deanosim@users.noreply.github.com>
2019-02-16 00:09:11 +1100

    Basic Calculator

---
+++ CSharp/Basic Calculator/Basic Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Basic_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine("Welcome to Dean's Calculator, Press enter to Continue or Exit to Quit.");
+                string str = Console.ReadLine();
+                int x;
+                if (str == "exit")
+                    break;
+                else
+                Console.Write("x =");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("y =");
+                int y = Convert.ToInt32(Console.ReadLine());
+
+                int sum = y + x;
+                Console.WriteLine("Result: {0}", sum);
+            }
+            while (true);
+        }
+    }
+}
